df093d3d6a232bccf8ddc8189477a7d9639e5a7f
fadrian06 <franyeradriansanchez@gmail.com>
2023-10-25 18:36:24 -0400

    Implemented persistence connection error handling

--- conexion/cone.php
+++ conexion/cone.php
@@ -1,9 +1,22 @@
 <?php
 
-$conexion = new mysqli("localhost", "root", "", "comunas");
-if ($conexion->connect_errno) {
-    echo "Fallo al conectar a MySQL: (" . $conexion->connect_errno . ") " . $mysqli->connect_error;
-}
-// echo $conexion->host_info . "\n";
+ini_set('error_log', __DIR__ . '/../logs/php_errors.log');
 
-?>
+try {
+	$env = require __DIR__ . '/../.env.php';
+
+	$conexion = new mysqli(
+		$env['DB_HOST'],
+		$env['DB_USER'],
+		$env['DB_PASSWORD'],
+		$env['DB_DATABASE']
+	);
+
+	$conexion->set_charset($env['DB_CHARSET']);
+
+	return $conexion;
+} catch (Throwable $error) {
+	error_log($error);
+	http_response_code(500);
+	exit;
+}
